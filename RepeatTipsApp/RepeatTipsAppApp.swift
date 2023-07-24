883c8d2986a198e13c6d7a91b934a0d0ce6fba93
kazukiomori <kazuki1110170501@gmail.com>
2023-07-24 21:40:19 +0900

    Initial Commit

---
+++ RepeatTipsApp/RepeatTipsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepeatTipsAppApp.swift
+//  RepeatTipsApp
+//
+//  Created by Kazuki Omori on 2023/07/24.
+//
+
+import SwiftUI
+
+@main
+struct RepeatTipsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
