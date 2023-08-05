ddb7a4b3dc5e093e7f20ae9f5bbe2e09123b1cda
kazukiomori <kazuki1110170501@gmail.com>
2023-08-06 00:37:14 +0900

    [Add]DateUtilクラス作成

---
+++ RepeatTipsApp/Util/DateUtil.swift
@@ -0,0 +1,24 @@
+//
+//  DateUtil.swift
+//  RepeatTipsApp
+//
+//  Created by Kazuki Omori on 2023/08/06.
+//
+
+import UIKit
+
+class DateUtils {
+    class func dateFromString(string: String, format: String) -> Date {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.dateFormat = format
+        return formatter.date(from: string)!
+    }
+
+    class func stringFromDate(date: Date, format: String) -> String {
+        let formatter: DateFormatter = DateFormatter()
+        formatter.calendar = Calendar(identifier: .gregorian)
+        formatter.dateFormat = format
+        return formatter.string(from: date)
+    }
+}
