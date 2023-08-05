f09c6ab27e227eca08539c244ca35cc8420f1932
kazukiomori <kazuki1110170501@gmail.com>
2023-08-06 00:16:46 +0900

    [Add]デバッグ用にCreateThemaViewをinitialViewに設定

--- RepeatTipsApp/RepeatTipsAppApp.swift
+++ RepeatTipsApp/RepeatTipsAppApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct RepeatTipsAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            CreateThemaView()
         }
     }
 }
