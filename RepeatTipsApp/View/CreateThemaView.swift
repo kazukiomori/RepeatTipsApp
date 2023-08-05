3a96b84f7b3a17bf103875c0814e4799feb33f02
kazukiomori <kazuki1110170501@gmail.com>
2023-08-06 00:17:59 +0900

    [Add]CreateThemaViewから画面遷移するよう修正

--- RepeatTipsApp/View/CreateThemaView.swift
+++ RepeatTipsApp/View/CreateThemaView.swift
@@ -10,55 +10,56 @@ import SwiftUI
 struct CreateThemaView: View {
     @State private var activie = false
     @State private var thema = ""
+    @State  private var path: [String] = []
     var body: some View {
-
-        VStack {
-            ZStack {
-                HStack {
-                    Spacer()
-                    Button (action: {
-                    }){
-                    }
-                }
-                Text("続けたいテーマを決める")
-                    .fontWeight(.bold)
-                    .font(.system(size: 20))
-            }
-            .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
-            .padding()
-            .overlay(Rectangle().stroke(Color.black.opacity(0.05), lineWidth: 2))
-            .background(Color.backgroundGray)
-            Text("ここで決めたことを\n毎日続けます")
-                .font(.system(size:30))
-                .bold()
-//                .padding(.bottom)
-            TextField("継続したいこと", text: $thema)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .padding(.top)
-                            .font(.system(size: 25))
-            Spacer()
+        
+        NavigationStack(path: $path) {
             VStack {
-                Button (action: {
-                    if thema == "" {
-                        return
-                    } else {
-                        activie = true
+                ZStack {
+                    HStack {
+                        Spacer()
+                        Button (action: {
+                        }){
+                        }
                     }
-                }){
-                    Text("次へ")
-                        .font(.largeTitle)
-                        .foregroundColor(Color.white)
+                    Text("続けたいテーマを決める")
+                        .fontWeight(.bold)
+                        .font(.system(size: 20))
                 }
+                .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
                 .padding()
-                .background(Color.blue)
-                .frame(width: 300, height: 50)
+                .overlay(Rectangle().stroke(Color.black.opacity(0.05), lineWidth: 2))
+                .background(Color.backgroundGray)
+                Text("ここで決めたことを\n毎日続けます")
+                    .font(.system(size:30))
+                    .bold()
+                //                .padding(.bottom)
+                TextField("継続したいこと", text: $thema)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.top)
+                    .font(.system(size: 25))
+                Spacer()
+                VStack {
+                    Button (action: {
+                        path.append(thema)
+                    }){
+                        Text("次へ")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    .padding()
+                    .background(Color.blue)
+                    .frame(width: 300, height: 50)
+                }
+                Spacer()
+                    .frame(height: 20)
             }
-            Spacer()
-                .frame(height: 20)
+            .edgesIgnoringSafeArea(.top)
+            .navigationDestination(for: String.self) { thema in
+                TimingView(path: $path)
+            }
         }
-        .edgesIgnoringSafeArea(.top)
-        }
-    
+    }
 }
 
 struct CreateThemaView_Previews: PreviewProvider {
