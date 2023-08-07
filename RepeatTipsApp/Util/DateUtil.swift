601107320593b8b834746e397b950e139b13f3a3
kazukiomori <kazuki1110170501@gmail.com>
2023-08-07 22:29:16 +0900

    [Fix]DateUtilクラスでDateをStringに変換するときに時間だけにするよう修正

--- RepeatTipsApp/Util/DateUtil.swift
+++ RepeatTipsApp/Util/DateUtil.swift
@@ -8,15 +8,17 @@
 import UIKit
 
 class DateUtils {
-    class func dateFromString(string: String, format: String) -> Date {
+    class func dateFromString(string: String) -> Date {
         let formatter: DateFormatter = DateFormatter()
+        let format = "yyyy/MM/dd HH:mm"
         formatter.calendar = Calendar(identifier: .gregorian)
         formatter.dateFormat = format
         return formatter.date(from: string)!
     }
 
-    class func stringFromDate(date: Date, format: String) -> String {
+    class func stringFromDate(date: Date) -> String {
         let formatter: DateFormatter = DateFormatter()
+        let format = "HH時mm分"
         formatter.calendar = Calendar(identifier: .gregorian)
         formatter.dateFormat = format
         return formatter.string(from: date)
