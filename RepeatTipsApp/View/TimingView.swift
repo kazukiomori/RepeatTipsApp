72cf2da498ef3b3e15485e4074a8bb63b189e398
kazukiomori <kazuki1110170501@gmail.com>
2023-08-07 22:28:27 +0900

    [Fix]TimingViewでNavigationPathをバインディングする

--- RepeatTipsApp/View/TimingView.swift
+++ RepeatTipsApp/View/TimingView.swift
@@ -8,57 +8,60 @@
 import SwiftUI
 
 struct TimingView: View {
-    @State private var time = Date()
-    @State private var thema = ""
+    @State var time = Date()
+    @State var thema: String
+    @Binding var path: [DestinationPath]
+    
     var body: some View {
-        
-        VStack {
-            ZStack {
-                HStack {
-                    Spacer()
-                    Button (action: {
-                    }){
-                    }
-                }
-                Text("いつやるか考える")
-                    .fontWeight(.bold)
-                    .font(.system(size: 20))
-            }
-            .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
-            .padding()
-            .overlay(Rectangle().stroke(Color.black.opacity(0.05), lineWidth: 2))
-            .background(Color.backgroundGray)
-            Text("毎日決まった時間に\n行動することが継続のコツです")
-                .font(.system(size:25))
-                .bold()
-            //                .padding(.bottom)
-            Form{
-                DatePicker("毎日やる時間", selection: $time, displayedComponents: .hourAndMinute)
-                    .datePickerStyle(.wheel)
-            }
-            Spacer()
             VStack {
-                Button (action: {
-                    
-                }){
-                    Text("次へ")
-                        .font(.largeTitle)
-                        .foregroundColor(Color.white)
+                ZStack {
+                    HStack {
+                        Spacer()
+                        Button (action: {
+                        }){
+                        }
+                    }
+                    Text("いつやるか考える")
+                        .fontWeight(.bold)
+                        .font(.system(size: 20))
                 }
-                .frame(width: 100, height: 20)
+                .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
                 .padding()
-                .background(Color.blue)
-                
+                .overlay(Rectangle().stroke(Color.black.opacity(0.05), lineWidth: 2))
+                .background(Color.backgroundGray)
+                Text("毎日決まった時間に\n行動することが継続のコツです")
+                    .font(.system(size:25))
+                    .bold()
+                //                .padding(.bottom)
+                Form{
+                    DatePicker("毎日やる時間", selection: $time, displayedComponents: .hourAndMinute)
+                        .datePickerStyle(.wheel)
+                }
+                Spacer()
+                VStack {
+                    Button (action: {
+                        path.append(DestinationPath.confirm(thema: thema, time: time))
+                    }){
+                        Text("次へ")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    .frame(width: 100, height: 20)
+                    .padding()
+                    .background(Color.blue)
+                    
+                }
+                Spacer()
+                    .frame(height: 20)
             }
-            Spacer()
-                .frame(height: 20)
+            .edgesIgnoringSafeArea(.top)
         }
-        .edgesIgnoringSafeArea(.top)
     }
-}
 
 struct TimingView_Previews: PreviewProvider {
+    @State  static var thema: String = ""
+    @State  static var path: [DestinationPath] = []
     static var previews: some View {
-        TimingView()
+        TimingView(thema: thema, path: $path)
     }
 }
