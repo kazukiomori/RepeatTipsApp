c9dacd542b6fb2ee14993056084efa2e407963ba
kazukiomori <kazuki1110170501@gmail.com>
2023-09-18 17:37:04 +0900

    [Fix]DateUtilクラスの関数をstatic関数に変更

--- RepeatTipsApp/Util/DateUtil.swift
+++ RepeatTipsApp/Util/DateUtil.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class DateUtils {
-    class func dateFromString(string: String) -> Date {
+    static func dateFromString(string: String) -> Date {
         let formatter: DateFormatter = DateFormatter()
         let format = "yyyy/MM/dd HH:mm"
         formatter.calendar = Calendar(identifier: .gregorian)
@@ -16,7 +16,7 @@ class DateUtils {
         return formatter.date(from: string)!
     }
 
-    class func stringFromDate(date: Date) -> String {
+    static func stringFromDate(date: Date) -> String {
         let formatter: DateFormatter = DateFormatter()
         let format = "HH時mm分"
         formatter.calendar = Calendar(identifier: .gregorian)
@@ -25,7 +25,7 @@ class DateUtils {
     }
     
     /// Date型の配列をData型に変換
-    func convertDatesToData(dates: [Date]) -> Data? {
+    static func convertDatesToData(dates: [Date]) -> Data? {
         do {
             let data = try NSKeyedArchiver.archivedData(withRootObject: dates, requiringSecureCoding: false)
             return data
@@ -36,7 +36,7 @@ class DateUtils {
     }
     
     /// Data型をDate型の配列に変換
-    func convertDataToDates(data: Data) -> [Date]? {
+    static func convertDataToDates(data: Data) -> [Date]? {
         do {
             if let dates = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? [Date] {
                 return dates
